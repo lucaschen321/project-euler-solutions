23734246253596b40cd84056212c4d5eb6b30c6a
Lucas Chen <lmc336@cornell.edu>
2016-12-23 16:54:20 -0600

    Added library helper functions

---
+++ python/eulerlib.py
@@ -0,0 +1,27 @@
+#
+# Helper functions for Project Euler
+# by Lucas Chen
+#
+
+import math
+
+# Given integer n return whether n is prime. 
+# Source: https://en.wikipedia.org/wiki/Primality_test#Simple_methods
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    else:
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+# Given integer n, list the primes <= n.
+def list_primes(n):
+    return [i for i in range(1, n + 1) if is_prime(i)]
